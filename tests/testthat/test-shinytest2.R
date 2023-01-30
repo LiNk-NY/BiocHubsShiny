1ca6320ec5286a6dcefc6bbe032e765ccaefe01e
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-01-30 16:39:47 -0500

    update tests, add delay for screenshot

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -9,19 +9,45 @@ test_that("{shinytest2} recording: BiocHubsShiny-EH", {
         width = 2259
     )
     app$set_inputs(hub = "ExperimentHub")
-    app$expect_screenshot(delay = 10)
+    app$expect_screenshot(delay = 30)
 })
 
 test_that("{shinytest2} recording: BiocHubsShiny-about", {
-    app <- AppDriver$new(variant = platform_variant(), name = "BiocHubsShiny-about",
-        height = 1256, width = 2259)
+    app <- AppDriver$new(
+        variant = platform_variant(),
+        name = "BiocHubsShiny-about",
+        height = 1256, width = 2259
+    )
     app$set_inputs(navbarID = "about")
     app$expect_screenshot()
 })
 
 test_that("{shinytest2} recording: BiocHubsShiny-selection", {
-    app <- AppDriver$new(variant = platform_variant(), name = "BiocHubsShiny-selection",
-        height = 1256, width = 2259)
-    app$set_inputs(code = "## Make sure BiocManager is installed\nif (!require('BiocManager', quietly = TRUE))\n    install.packages('BiocManager')\n\n## Make sure AnnotationHub is installed\nif (!require(\"AnnotationHub\", quietly = TRUE))\n    BiocManager::install(\"AnnotationHub\")\n\n## Use this code to download the resource\nlibrary(\"AnnotationHub\")\nhub <- AnnotationHub()\n\n## Select rows in the table\nhub[['AH5012']]\nhub[['AH5013']]\nhub[['AH5014']]")
-    app$expect_screenshot(delay = 10)
+    app <- AppDriver$new(
+        variant = platform_variant(),
+        name = "BiocHubsShiny-selection",
+        height = 1256, width = 2259
+    )
+    app$set_inputs(tbl_rows_selected = 1, allow_no_input_binding_ = TRUE)
+    app$set_inputs(
+        tbl_cell_clicked = c("1", "0", "AH5012"),
+        allow_no_input_binding_ = TRUE,
+        priority_ = "event"
+    )
+    app$set_inputs(tbl_rows_selected = c(1, 2), allow_no_input_binding_ = TRUE)
+    app$set_inputs(
+        tbl_cell_clicked = c("2", "0", "AH5013"),
+        allow_no_input_binding_ = TRUE,
+        priority_ = "event"
+    )
+    app$set_inputs(
+        tbl_rows_selected = c(1, 2, 3), allow_no_input_binding_ = TRUE
+    )
+    app$set_inputs(
+        tbl_cell_clicked = c("3", "0", "AH5014"),
+        allow_no_input_binding_ = TRUE,
+        priority_ = "event"
+    )
+    app$wait_for_idle()
+    app$expect_screenshot(delay = 30)
 })
