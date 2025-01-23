5a1fd2017d817ebcb84471dfe9254e568d62354d
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-01-23 14:10:42 -0500

    add deployment configuration for shinyapps.io

--- app.R
+++ app.R
@@ -1,3 +1,11 @@
 # Launch the ShinyApp (Do not remove this comment)
 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+options(repos = BiocManager::repositories())
+
+if (!requireNamespace("BiocHubsShiny", quietly = TRUE))
+    BiocManager::install("BiocHubsShiny")
+
 BiocHubsShiny::BiocHubsShiny() # add parameters here (if any)
